d2a583e7e184a0ae706fb80294ec1398d1d0ebae
Matt Black <matt.black7@gmail.com>
2014-11-03 20:20:26 -0500

    update setup.py

---
+++ DESCRIPTION.rst
@@ -0,0 +1,8 @@
+Decision Curve Analysis
+=======================
+
+"Decision curve analysis is a simple method for evaluating prediction models, diagnostic tests, and molecular markers."
+
+The method was first published as:
+
+Vickers AJ, Elkin, EB. Decision curve analysis: a novel method for evaluating prediction models. Medical Decision Making. 2006 Nov-Dec;26(6):565-74
--- setup.py
+++ setup.py
@@ -4,20 +4,33 @@ Setup script for the dcapy module
 
 Author: Matthew Black
 """
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
+
+this_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_dir, "DESCRIPTION.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(name='dcapy',
+
       version='0.1',
+
       description='Decision Curve Analysis library',
+      long_description=long_description,
+
       license='GPLv3+',
       install_requires=[
           'pandas'
           ],
+
       author='Matthew Black',
       author_email='matt.black7@gmail.com',
       url='http://matt-black.github.io',
+
+      packages=find_packages(exclude=['test']),
+
       keywords='statistics analysis',
-      packages=['dcapy'],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
